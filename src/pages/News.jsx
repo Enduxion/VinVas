faf834f03cda17b7fd94c8d7fcb4320bd9b2d308
Enduxion <endux.pg@gmail.com>
2023-06-04 19:50:31 +0545

    Changed the data to better make sense;

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import NewsContainer from "../components/NewsContainer";
 import HeaderImage from "../assets/images/TestPicture4.jpg";
 import TestImage from "../assets/images/TestPicture2.jpg";
+import TestImage2 from "../assets/images/TestPicture3.jpg";
 
 import "../css/NewsStyle.css";
 
@@ -10,34 +11,37 @@ export default function News(props) {
   // Temporary Data
   const postData = [
     {
-      title: "Pawan's Website",
+      id: 1,
+      title: "Pawan Website",
       date: "06/04/2023",
-      description: "Pawan gurung made this website a long time ago when he was a kid. He likes to program and likes to beat everybody. Thank you!",
+      description: "This is a dummy text! Vinvas website 2023, this can be modified by fetching data from api",
       img: TestImage
     },
     {
-      title: "Nolan's Website",
-      date: "09/04/2023",
-      description: "Nolan made this website a short time ago when he was a old man. He likes to program and likes to beat everybody. Thank you!",
+      id: 2,
+      title: "BackEnd Not found (lol)",
+      date: "09/04/2022",
+      description: "I was moving and suddenly the earth started moving with me, haha it's a joke, I know right? Sounded Realistic",
       img: HeaderImage
     },
     {
-      title: "Pawan's Website",
-      date: "06/04/2023",
-      description: "Pawan gurung made this website a long time ago when he was a kid. He likes to program and likes to beat everybody. Thank you!",
-      img: TestImage
+      id: 3,
+      title: "Hiya",
+      date: "07/06/2021",
+      description: "Thank you everybody for joining me! ",
+      img: TestImage2
     }
     ,
     {
-      title: "Pawan's Website",
-      date: "06/04/2023",
-      description: "Pawan gurung made this website a long time ago when he was a kid. He likes to program and likes to beat everybody. Thank you",
-      // img: HeaderImage
+      id: 4,
+      title: "Hellowww",
+      date: "06/04/2019",
+      description: "This one contains no image and we can still render the data.... We should prolly link it, right now you can't click it hehe"
     }
   ];
-  let allNews = postData.map((newsData, index) => {
+  let allNews = postData.map((newsData) => {
     return (
-      <NewsContainer key={index} title={newsData.title} date={newsData.date} description={newsData.description} img={newsData.img} />
+      <NewsContainer key={newsData.id} title={newsData.title} date={newsData.date} description={newsData.description} img={newsData.img} />
     );
   });
   return (
