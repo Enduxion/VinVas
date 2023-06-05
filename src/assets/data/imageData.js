0c2ce1b386617fc183eb7093d2b6eeb3163b2665
Enduxion <endux.pg@gmail.com>
2023-06-05 06:43:18 +0545

    Added more news! ~ Made See more feature; ~ Added a data folder (temp);

---
+++ src/assets/data/imageData.js
@@ -0,0 +1,6 @@
+import image1 from "../images/TestPicture.jpg"
+import image2 from "../images/TestPicture2.jpg"
+import image3 from "../images/TestPicture3.jpg"
+import image4 from "../images/TestPicture4.jpg"
+
+export default [image1, image2, image3, image4];
--- src/components/NewsContainer.jsx
+++ src/components/NewsContainer.jsx
@@ -1,12 +1,48 @@
+import { useEffect, useState } from "react";
 import "../css/NewsContainerStyle.css";
+
+const MAX_LINE_VAL = 150;
+
 export default function NewsContainer(props) {
+  const [descriptionText, setDescriptionText] = useState("");
+  const [fullDescriptionShow, setFullDescriptionShow] = useState(false);
+  // Toggle whether to show full description of not
+  function toggleFullDescriptionShow() {
+    setFullDescriptionShow((prevValue) => !prevValue);
+  }
+  // Set the "see more" trim when the program first starts
+  useEffect(() => {
+    if (props.description.trim().length > MAX_LINE_VAL) {
+      setDescriptionText(props.description.substr(0, (MAX_LINE_VAL - 1)).trim() + "... ");
+    } else {
+      setDescriptionText(props.description);
+      setFullDescriptionShow(true);
+    }
+  }, []);
+  // set the description on showing full or not showing full
+  useEffect(() => {
+    fullDescriptionShow
+      ? setDescriptionText(props.description)
+      : setDescriptionText(props.description.substr(0, (MAX_LINE_VAL - 1)).trim() + "... ");
+  }, [fullDescriptionShow]);
+  // return
   return (
     <div className="NewsContainer">
       <div className="NewsContainer--titleBar">
         <span className="NewsContainer--title">{props.title}</span>
         <span className="NewsContainer--uploadDate">{props.date}</span>
       </div>
-      <div className="NewsContainer--descriptionBox">{props.description}</div>
+      <div className="NewsContainer--descriptionBox">
+        {descriptionText}
+        {props.description.length > MAX_LINE_VAL ? (
+          <span className="btn--text" onClick={toggleFullDescriptionShow}>
+            {" "}
+            See {fullDescriptionShow ? "less" : "more"}
+          </span>
+        ) : (
+          ""
+        )}
+      </div>
       {props.img ? (
         <img className="NewsContainer--image" src={props.img}></img>
       ) : (
