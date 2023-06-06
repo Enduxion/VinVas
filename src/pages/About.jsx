66df0cf9742fea91b5ea49d6cc319efcd408e1bf
Enduxion <endux.pg@gmail.com>
2023-06-06 22:45:58 +0545

    Made AboutUs page; ~ Added a aboutUsData for storing the description only;

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,9 @@
 import NavBar from "../components/NavBar";
+import AsyncImage from "../components/AsyncImage";
+
+import aboutUsData from "../assets/data/aboutUsData";
+import headerImage from "../assets/images/TestPicture4.jpg";
+import "../css/aboutStyle.css";
 
 export default function About(props) {
   return (
@@ -8,6 +13,15 @@ export default function About(props) {
         darkMode={props.darkMode}
         toggleDarkMode={props.toggleDarkMode}
       />
+      <div className="About--main">
+        <div className="About--title">Shree Vindhya Vasini Seconday School</div>
+        <div className="About--quote">- "Discipline And Dedication For Quality Education..."</div>
+        <div className="About--imageContainer">
+          <AsyncImage className="About--image" src={headerImage} />
+        </div>
+        <div className="About--subTitle">Introduction</div>
+        <div className="About--description">{aboutUsData}</div>
+      </div>
     </>
   );
 }
